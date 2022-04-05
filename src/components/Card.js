e682a05114d1028e934c1b4cba52f3610d899900
syedraahim <syedraahim98@gmail.com>
2022-04-05 19:46:55 +0500

    always active bingo condition

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import { Card, CardTitle, CardBody, CardText } from 'reactstrap';
 
  const BingoTile = ({ id, key, onToggle, isSet , data}) => {
     return (
-        <Card onClick={id !== 12 ? onToggle : null} className={`tile ${isSet ? "tile--set col card border-secondary" : "col card border-secondary"}`}>
+        <Card onClick={id != 12 ? onToggle : null} className={`tile ${isSet ? "tile--set col card border-secondary" : "col card border-secondary"}`}>
           <CardBody>
             {id != 12 && <CardTitle>{id}</CardTitle>}
             <CardText>{data}</CardText>    
